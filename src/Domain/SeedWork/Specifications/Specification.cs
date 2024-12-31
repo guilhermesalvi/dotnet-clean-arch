8af7b330d79c0a61817320d5ac2b1e64b757a49a
Guilherme Salvi <guisalvi@outlook.com>
2024-12-31 12:46:30 -0300

    refctor: upgrade to dotnet 9

--- src/Domain/SeedWork/Specifications/Specification.cs
+++ src/Domain/SeedWork/Specifications/Specification.cs
@@ -12,18 +12,11 @@ public abstract class Specification<T>
 
     public abstract Expression<Func<T, bool>> ToExpression();
 
-    public Specification<T> And(Specification<T> specification)
-    {
-        return new AndSpecification<T>(this, specification);
-    }
+    public Specification<T> And(Specification<T> specification) =>
+        new AndSpecification<T>(this, specification);
 
-    public Specification<T> Or(Specification<T> specification)
-    {
-        return new OrSpecification<T>(this, specification);
-    }
+    public Specification<T> Or(Specification<T> specification) =>
+        new OrSpecification<T>(this, specification);
 
-    public Specification<T> Not()
-    {
-        return new NotSpecification<T>(this);
-    }
+    public Specification<T> Not() => new NotSpecification<T>(this);
 }
